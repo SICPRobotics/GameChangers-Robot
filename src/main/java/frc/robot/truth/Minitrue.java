c18376a543b144746ae98d3c018a38605a7820d1
unknown <ignatiusrobotics@gmail.com>
2020-09-24 16:51:09 -0500

    Added pose to Truth & Minitrue

--- src/main/java/frc/robot/truth/Minitrue.java
+++ src/main/java/frc/robot/truth/Minitrue.java
@@ -1,5 +1,6 @@
 package frc.robot.truth;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
 import frc.robot.pi_client.PiClient;
 import frc.robot.pi_client.VisionObserver;
 import frc.robot.pi_client.VisionStatus;
@@ -11,21 +12,32 @@ import frc.robot.pi_client.VisionStatus;
 public class Minitrue implements VisionObserver {
 
     private final PiClient piClient;
+    private Truth truth;
 
     public Minitrue(PiClient piClient) {
         this.piClient = piClient;
         this.piClient.onVisionUpdate(this);
+        truth = new Truth();
     }
 
+    /**
+     * Called when the vision sends an update.
+     */
     public void onVisionUpdate(VisionStatus status) {
         // Do stuff with the vision data
     }
 
     /**
-     * TODO
-     * @return Return's the robot's truth.
+     * @return The robot's truth (aka state).
      */
     public Truth getTruth() {
-        return null;
+        return truth;
+    }
+
+    /**
+     * @return The robot's pose taken from the robot's truth.
+     */
+    public Pose2d getPose() {
+        return truth.pose;
     }
 }
--- src/main/java/frc/robot/truth/Truth.java
+++ src/main/java/frc/robot/truth/Truth.java
@@ -1,8 +1,10 @@
 package frc.robot.truth;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+
 /**
  * TODO The truth for the robot. Contains information about its current state, such as position, rotation, etc.
  */
 public class Truth {
-
+    public Pose2d pose;
 }
