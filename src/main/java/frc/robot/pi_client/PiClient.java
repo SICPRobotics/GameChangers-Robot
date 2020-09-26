3a60b5ba49fa8bc4cce360388c34d2e47005ac0d
unknown <ignatiusrobotics@gmail.com>
2020-09-26 11:47:29 -0500

    Removed unused import

--- src/main/java/frc/robot/pi_client/PiClient.java
+++ src/main/java/frc/robot/pi_client/PiClient.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
